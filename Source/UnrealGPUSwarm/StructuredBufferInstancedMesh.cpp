3696bee3c66608ba202ab6b87256354bc5eb260c
Timothy Davison <timtimmy@gmail.com>
2020-03-25 12:27:10 -0600

    Copying code from the ISMC

--- Source/UnrealGPUSwarm/StructuredBufferInstancedMesh.cpp
+++ Source/UnrealGPUSwarm/StructuredBufferInstancedMesh.cpp
@@ -17,8 +17,11 @@ class UNREALGPUSWARM_API FStructredBufferInstancedMeshProxy : public FStaticMesh
 public:
 	FStructredBufferInstancedMeshProxy(UStaticMeshComponent* InComponent, bool bForceLODsShareStaticLighting) 
 		: FStaticMeshSceneProxy(InComponent, bForceLODsShareStaticLighting)
+		, InstancedRenderData(InComponent, InFeatureLevel)
 	{
+		bVFRequiresPrimitiveUniformBuffer = true;
 
+		SetupProxy(InComponent);
 	}
 
 	virtual ~FStructredBufferInstancedMeshProxy()
@@ -33,20 +36,120 @@ public:
 	}
 
 	virtual void GetDynamicMeshElements(const TArray<const FSceneView*>& Views, const FSceneViewFamily& ViewFamily, uint32 VisibilityMap, FMeshElementCollector& Collector) const override;
+	
+	/** Sets up a FMeshBatch for a specific LOD and element. */
+	virtual bool GetMeshElement(int32 LODIndex, int32 BatchIndex, int32 ElementIndex, uint8 InDepthPriorityGroup, bool bUseSelectionOutline, bool bAllowPreCulledIndices, FMeshBatch& OutMeshBatch) const override;
 
+	/** Common path for the Get*MeshElement functions */
+	void SetupInstancedMeshBatch(int32 LODIndex, int32 BatchIndex, FMeshBatch& OutMeshBatch) const;
+
+	void SetupProxy(UInstancedStaticMeshComponent* InComponent);
 };
 
 void FStructredBufferInstancedMeshProxy::GetDynamicMeshElements(const TArray<const FSceneView*>& Views, const FSceneViewFamily& ViewFamily, uint32 VisibilityMap, FMeshElementCollector& Collector) const
 {
+	const bool bSelectionRenderEnabled = GIsEditor && ViewFamily.EngineShowFlags.Selection;
 
+	// If the first pass rendered selected instances only, we need to render the deselected instances in a second pass
+	const int32 NumSelectionGroups = (bSelectionRenderEnabled && bHasSelectedInstances) ? 2 : 1;
+
+	const FInstancingUserData* PassUserData[2] =
+	{
+		bHasSelectedInstances && bSelectionRenderEnabled ? &UserData_SelectedInstances : &UserData_AllInstances,
+		&UserData_DeselectedInstances
+	};
+
+	bool BatchRenderSelection[2] =
+	{
+		bSelectionRenderEnabled && IsSelected(),
+		false
+	};
+
+	const bool bIsWireframe = ViewFamily.EngineShowFlags.Wireframe;
+
+	for (int32 ViewIndex = 0; ViewIndex < Views.Num(); ViewIndex++)
+	{
+		if (VisibilityMap & (1 << ViewIndex))
+		{
+			const FSceneView* View = Views[ViewIndex];
+
+			for (int32 SelectionGroupIndex = 0; SelectionGroupIndex < NumSelectionGroups; SelectionGroupIndex++)
+			{
+				const int32 LODIndex = GetLOD(View);
+				const FStaticMeshLODResources& LODModel = StaticMesh->RenderData->LODResources[LODIndex];
+
+				for (int32 SectionIndex = 0; SectionIndex < LODModel.Sections.Num(); SectionIndex++)
+				{
+					const int32 NumBatches = GetNumMeshBatches();
+
+					for (int32 BatchIndex = 0; BatchIndex < NumBatches; BatchIndex++)
+					{
+						FMeshBatch& MeshElement = Collector.AllocateMesh();
+
+						if (GetMeshElement(LODIndex, BatchIndex, SectionIndex, GetDepthPriorityGroup(View), BatchRenderSelection[SelectionGroupIndex], true, MeshElement))
+						{
+							//@todo-rco this is only supporting selection on the first element
+							MeshElement.Elements[0].UserData = PassUserData[SelectionGroupIndex];
+							MeshElement.Elements[0].bUserDataIsColorVertexBuffer = false;
+							MeshElement.bCanApplyViewModeOverrides = true;
+							MeshElement.bUseSelectionOutline = BatchRenderSelection[SelectionGroupIndex];
+							MeshElement.bUseWireframeSelectionColoring = BatchRenderSelection[SelectionGroupIndex];
+
+							if (View->bRenderFirstInstanceOnly)
+							{
+								for (int32 ElementIndex = 0; ElementIndex < MeshElement.Elements.Num(); ElementIndex++)
+								{
+									MeshElement.Elements[ElementIndex].NumInstances = FMath::Min<uint32>(MeshElement.Elements[ElementIndex].NumInstances, 1);
+								}
+							}
+
+							Collector.AddMesh(ViewIndex, MeshElement);
+							INC_DWORD_STAT_BY(STAT_StaticMeshTriangles, MeshElement.GetNumPrimitives());
+						}
+					}
+				}
+			}
+		}
+	}
 }
 
+bool FStructredBufferInstancedMeshProxy::GetMeshElement(int32 LODIndex, int32 BatchIndex, int32 ElementIndex, uint8 InDepthPriorityGroup, bool bUseSelectionOutline, bool bAllowPreCulledIndices, FMeshBatch& OutMeshBatch) const
+{
+	if (LODIndex < InstancedRenderData.VertexFactories.Num() && FStaticMeshSceneProxy::GetMeshElement(LODIndex, BatchIndex, ElementIndex, InDepthPriorityGroup, bUseSelectionOutline, bAllowPreCulledIndices, OutMeshBatch))
+	{
+		SetupInstancedMeshBatch(LODIndex, BatchIndex, OutMeshBatch);
+		return true;
+	}
+	return false;
+}
 
+void FStructredBufferInstancedMeshProxy::SetupInstancedMeshBatch(int32 LODIndex, int32 BatchIndex, FMeshBatch& OutMeshBatch) const
+{
+	const bool bInstanced = GRHISupportsInstancing;
+	OutMeshBatch.VertexFactory = &InstancedRenderData.VertexFactories[LODIndex];
 
+	FMeshBatchElement& BatchElement0 = OutMeshBatch.Elements[0];
 
+	BatchElement0.UserData = (void*)&UserData_AllInstances;
+	BatchElement0.bUserDataIsColorVertexBuffer = false;
+	BatchElement0.InstancedLODIndex = LODIndex;
+	BatchElement0.UserIndex = 0;
+	BatchElement0.bIsInstancedMesh = bInstanced;
+	BatchElement0.PrimitiveUniformBuffer = GetUniformBuffer();
 
+	// we are assuming modern hardware. If we don't have instancing, this entire class is pointless
+	if (!bInstanced)
+		return;
 
+	const uint32 NumInstances = InstancedRenderData.PerInstanceRenderData->InstanceBuffer.GetNumInstances();
 
+	BatchElement0.NumInstances = NumInstances;
+}
+
+void FStructredBufferInstancedMeshProxy::SetupProxy(UInstancedStaticMeshComponent* InComponent)
+{
+
+}
 
 FPrimitiveSceneProxy* UStructuredBufferInstancedMesh::CreateSceneProxy()
 {
