50695d1d008ecefe7a7730565f5d0756c6e5d063
Timothy Davison <timtimmy@gmail.com>
2020-03-25 12:47:10 -0600

    More work.

--- Source/UnrealGPUSwarm/StructuredBufferInstancedMesh.cpp
+++ Source/UnrealGPUSwarm/StructuredBufferInstancedMesh.cpp
@@ -4,6 +4,171 @@
 #include "StructuredBufferInstancedMesh.h"
 
 
+/** A vertex buffer of positions. */
+class FStructuredBufferMeshInstanceBuffer : public FRenderResource
+{
+public:
+
+	/** Default constructor. */
+	FStructuredBufferMeshInstanceBuffer(ERHIFeatureLevel::Type InFeatureLevel, bool InRequireCPUAccess);
+
+	/** Destructor. */
+	~FStructuredBufferMeshInstanceBuffer();
+
+	/**
+	 * Initializes the buffer with the component's data.
+	 * @param Other - instance data, this call assumes the memory, so this will be empty after the call
+	 */
+	ENGINE_API void InitFromPreallocatedData(FStaticMeshInstanceData& Other);
+	ENGINE_API void UpdateFromCommandBuffer_Concurrent(FInstanceUpdateCmdBuffer& CmdBuffer);
+
+	/**
+	 * Specialized assignment operator, only used when importing LOD's.
+	 */
+	void operator=(const FStaticMeshInstanceBuffer &Other);
+
+	// Other accessors.
+	FORCEINLINE uint32 GetNumInstances() const
+	{
+		return InstanceData->GetNumInstances();
+	}
+
+	FORCEINLINE  void GetInstanceTransform(int32 InstanceIndex, FMatrix& Transform) const
+	{
+		InstanceData->GetInstanceTransform(InstanceIndex, Transform);
+	}
+
+	FORCEINLINE  void GetInstanceShaderValues(int32 InstanceIndex, FVector4(&InstanceTransform)[3], FVector4& InstanceLightmapAndShadowMapUVBias, FVector4& InstanceOrigin) const
+	{
+		InstanceData->GetInstanceShaderValues(InstanceIndex, InstanceTransform, InstanceLightmapAndShadowMapUVBias, InstanceOrigin);
+	}
+
+	FORCEINLINE FStaticMeshInstanceData* GetInstanceData() const
+	{
+		return InstanceData.Get();
+	}
+
+	// FRenderResource interface.
+	virtual void InitRHI() override;
+	virtual void ReleaseRHI() override;
+	virtual void InitResource() override;
+	virtual void ReleaseResource() override;
+	virtual FString GetFriendlyName() const override { return TEXT("Static-mesh instances"); }
+	SIZE_T GetResourceSize() const;
+
+	void BindInstanceVertexBuffer(const class FVertexFactory* VertexFactory, struct FInstancedStaticMeshDataType& InstancedStaticMeshData) const;
+
+public:
+	/** The vertex data storage type */
+	TSharedPtr<FStaticMeshInstanceData, ESPMode::ThreadSafe> InstanceData;
+
+	/** Keep CPU copy of instance data*/
+	bool RequireCPUAccess;
+
+private:
+	class FInstanceOriginBuffer : public FVertexBuffer
+	{
+		virtual FString GetFriendlyName() const override { return TEXT("FInstanceOriginBuffer"); }
+	} InstanceOriginBuffer;
+	FShaderResourceViewRHIRef InstanceOriginSRV;
+
+	class FInstanceTransformBuffer : public FVertexBuffer
+	{
+		virtual FString GetFriendlyName() const override { return TEXT("FInstanceTransformBuffer"); }
+	} InstanceTransformBuffer;
+	FShaderResourceViewRHIRef InstanceTransformSRV;
+
+	class FInstanceLightmapBuffer : public FVertexBuffer
+	{
+		virtual FString GetFriendlyName() const override { return TEXT("FInstanceLightmapBuffer"); }
+	} InstanceLightmapBuffer;
+	FShaderResourceViewRHIRef InstanceLightmapSRV;
+
+	/** Delete existing resources */
+	void CleanUp();
+
+	void CreateVertexBuffer(FResourceArrayInterface* InResourceArray, uint32 InUsage, uint32 InStride, uint8 InFormat, FVertexBufferRHIRef& OutVertexBufferRHI, FShaderResourceViewRHIRef& OutInstanceSRV);
+
+	/**  */
+	void UpdateFromCommandBuffer_RenderThread(FInstanceUpdateCmdBuffer& CmdBuffer);
+};
+
+/*-----------------------------------------------------------------------------
+	FPerInstanceRenderData
+	Holds render data that can persist between scene proxy reconstruction
+-----------------------------------------------------------------------------*/
+struct FStructredBufferPerInstanceRenderData
+{
+	// Should be always constructed on main thread
+	FPerInstanceRenderData(FStaticMeshInstanceData& Other, ERHIFeatureLevel::Type InFeaureLevel, bool InRequireCPUAccess);
+	~FPerInstanceRenderData();
+
+	/**
+	 * Call to update the Instance buffer with pre allocated data without recreating the FPerInstanceRenderData
+	 * @param InComponent - The owning component
+	 * @param InOther - The Instance data to copy into our instance buffer
+	 */
+	ENGINE_API void UpdateFromPreallocatedData(FStaticMeshInstanceData& InOther);
+
+	/**
+	*/
+	ENGINE_API void UpdateFromCommandBuffer(FInstanceUpdateCmdBuffer& CmdBuffer);
+
+	/** Hit proxies for the instances */
+	TArray<TRefCountPtr<HHitProxy>>		HitProxies;
+
+	/** cached per-instance resource size*/
+	SIZE_T								ResourceSize;
+
+	/** Instance buffer */
+	FStaticMeshInstanceBuffer			InstanceBuffer;
+	TSharedPtr<FStaticMeshInstanceData, ESPMode::ThreadSafe> InstanceBuffer_GameThread;
+};
+
+
+
+class FStructredBufferInstancedMeshRenderData
+{
+public:
+
+	FStructredBufferInstancedMeshRenderData(UStructuredBufferInstancedMesh* InComponent, ERHIFeatureLevel::Type InFeatureLevel)
+		: Component(InComponent)
+		, PerInstanceRenderData(InComponent->PerInstanceRenderData)
+		, LODModels(Component->GetStaticMesh()->RenderData->LODResources)
+		, FeatureLevel(InFeatureLevel)
+	{
+		check(PerInstanceRenderData.IsValid());
+
+		// Allocate the vertex factories for each LOD
+		InitVertexFactories();
+	}
+
+	void ReleaseResources(FSceneInterface* Scene, const UStaticMesh* StaticMesh)
+	{
+		for (int32 LODIndex = 0; LODIndex < VertexFactories.Num(); LODIndex++)
+		{
+			VertexFactories[LODIndex].ReleaseResource();
+		}
+	}
+
+	/** Source component */
+	UStructuredBufferInstancedMesh* Component;
+
+	/** Per instance render data, could be shared with component */
+	TSharedPtr<FStructredBufferPerInstanceRenderData, ESPMode::ThreadSafe> PerInstanceRenderData;
+
+	/** Vertex factory */
+	TIndirectArray<FInstancedStaticMeshVertexFactory> VertexFactories;
+
+	/** LOD render data from the static mesh. */
+	TIndirectArray<FStaticMeshLODResources>& LODModels;
+
+	/** Feature level used when creating instance data */
+	ERHIFeatureLevel::Type FeatureLevel;
+
+private:
+	void InitVertexFactories();
+};
 
 
 
@@ -12,10 +177,11 @@
 class UNREALGPUSWARM_API FStructredBufferInstancedMeshProxy : public FStaticMeshSceneProxy
 {
 
-
+public:
+	FStructredBufferInstancedMeshRenderData InstancedRenderData;
 
 public:
-	FStructredBufferInstancedMeshProxy(UStaticMeshComponent* InComponent, bool bForceLODsShareStaticLighting) 
+	FStructredBufferInstancedMeshProxy(UStructuredBufferInstancedMesh* InComponent, bool bForceLODsShareStaticLighting)
 		: FStaticMeshSceneProxy(InComponent, bForceLODsShareStaticLighting)
 		, InstancedRenderData(InComponent, InFeatureLevel)
 	{
