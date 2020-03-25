b4fd1c9660beeb2be2f9fb4073a472299214a6c6
Timothy Davison <timtimmy@gmail.com>
2020-03-24 20:21:31 -0600

    Let's try inheriting from UStaticMeshComponent.

--- Source/UnrealGPUSwarm/StructuredBufferInstancedMesh.h
+++ Source/UnrealGPUSwarm/StructuredBufferInstancedMesh.h
@@ -10,26 +10,23 @@
  * 
  */
 UCLASS()
-class UNREALGPUSWARM_API UStructuredBufferInstancedMesh : public UPrimitiveComponent
+class UNREALGPUSWARM_API UStructuredBufferInstancedMesh : public UStaticMeshComponent
 {
 	GENERATED_BODY()
 	
 private:
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = StaticMesh, ReplicatedUsing = OnRep_StaticMesh, meta = (AllowPrivateAccess = "true"))
-	class UStaticMesh * StaticMesh;
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = StaticMesh, ReplicatedUsing = OnRep_StaticMesh, meta = (AllowPrivateAccess = "true"))
+	//class UStaticMesh * StaticMesh;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = StaticMesh, ReplicatedUsing = OnRep_StaticMesh, meta = (AllowPrivateAccess = "true"))
-	class UMaterialInterface * Material;
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = StaticMesh, ReplicatedUsing = OnRep_StaticMesh, meta = (AllowPrivateAccess = "true"))
+	//class UMaterialInterface * Material;
 
 public:
 	//~ Begin UPrimitiveComponent Interface.
 	virtual FPrimitiveSceneProxy* CreateSceneProxy() override;
 	//~ End UPrimitiveComponent Interface.
 
-	//~ Begin USceneComponent Interface.
-	virtual FBoxSphereBounds CalcBounds(const FTransform& LocalToWorld) const override;
-	//~ Begin USceneComponent Interface.
-
-protected:
-	UStaticMesh * GetStaticMesh();
 };
+
+
+
