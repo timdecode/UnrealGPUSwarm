55bbc769963f01b18908920b58288c82b0e81161
Timothy Davison <timtimmy@gmail.com>
2020-03-26 14:00:26 -0600

    Added positions and directions buffer refs.

--- Source/UnrealGPUSwarm/InstanceBufferMeshComponent.h
+++ Source/UnrealGPUSwarm/InstanceBufferMeshComponent.h
@@ -125,6 +125,9 @@ class UNREALGPUSWARM_API UInstanceBufferMeshComponent : public UStaticMeshCompon
 	/** Needs implementation in InstancedStaticMesh.cpp to compile UniquePtr for forward declared class */
 	UInstanceBufferMeshComponent(FVTableHelper& Helper);
 	virtual ~UInstanceBufferMeshComponent();
+
+	FStructuredBufferRHIRef positionsBuffer;
+	FStructuredBufferRHIRef directionsBuffer;
 	
 	/** Array of instances, bulk serialized. */
 	UPROPERTY(EditAnywhere, SkipSerialization, DisplayName="Instances", Category=Instances, meta=(MakeEditWidget=true, EditFixedOrder))
